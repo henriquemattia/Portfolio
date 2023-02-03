bbde5a766a9df78f36cf649f7447a6a6c50f0122
henriquemattia <mattia.henrique@gmail.com>
2023-02-03 17:08:01 -0300

    changing tecnologies cards

--- pages/index.js
+++ pages/index.js
@@ -8,20 +8,21 @@ import { IoLogoWhatsapp } from "react-icons/io"
 import { BsFillMoonStarsFill } from "react-icons/bs";
 
 import deved from "../public/dev-ed-wave.png";
-
-import code from "../public/code.png";
-import design from "../public/design.png";
-import consulting from "../public/consulting.png";
 import Image from "next/image";
-import web1 from "../public/web1.png";
-import web2 from "../public/web2.png";
-import web3 from "../public/web3.png";
-import web4 from "../public/web4.png";
-import web5 from "../public/web5.png";
-import web6 from "../public/web6.png";
+
+import node from "../public/tecnologias/node.png";
+import js from "../public/tecnologias/js.png";
+import react from "../public/tecnologias/react.png";
+
+import web1 from "../public/portfolio/web1.png";
+import web2 from "../public/portfolio/web2.png";
+import web3 from "../public/portfolio/web3.png";
+import web4 from "../public/portfolio/web4.png";
+import web5 from "../public/portfolio/web5.png";
+import web6 from "../public/portfolio/web6.png";
 
 export default function Home() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
 
   return (
     <div className={darkMode ? "dark" : ""}>
@@ -59,8 +60,7 @@ export default function Home() {
               Full stack Developer
             </h3>
             <p className="font-semibold  text-lg pt-9 pb-4 leading-8 text-zinc-800 opacity-80 dark:text-zinc-300 max-w-xl mx-auto  md:text-lg ">
-              Freelancer providing services for programming and design content
-              needs. Join me down below and let's get cracking!
+              Desenvolvedor que presta serviços para necessidades de conteúdo de programação. Junte-se a mim abaixo e vamos lá!
             </p>
             <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
               <a href="https://github.com/henriquemattia" target='_blank'> <AiFillGithub /> </a>
@@ -70,71 +70,95 @@ export default function Home() {
               <a href="https://wa.me/5549998339450" target='_blank'> <IoLogoWhatsapp /> </a>
             </div>
             <div className="flex justify-center items-center">
-            <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden bg-gray-800 mt-20 md:h-96 md:w-96 ">
-              <Image src={deved} layout="fill" objectFit="cover" />
-            </div>
+              <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden bg-gray-800 mt-20 md:h-96 md:w-96 ">
+                <Image src={deved} layout="fill" objectFit="cover" />
+              </div>
             </div>
           </div>
         </section>
         <section>
           <div>
-            <h3 className="font-bold text-black text-3xl py-1 opacity-80 dark:text-white ">Services I offer</h3>
+            <h3 className="font-bold text-black text-3xl py-1 opacity-80 dark:text-white ">Quem sou eu?</h3>
             <p className="font-semibold text-md py-2 leading-8 text-zinc-800 opacity-80 dark:text-zinc-300">
-              Since the beginning of my journey as a freelance designer and
-              developer, I've done remote work for
-              <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500">startups </span>
-              and collaborated with talanted people to create digital products
-              for both business and consumer use.
+              Sou um jovem desenvolvedor, e entusiasta da <span className="text-teal-500"> tecnologia</span>, venho apriomorando minhas abilidades em desenvolvimento web por volta de um
+              <span className="text-teal-500"> ano </span>
+
             </p>
             <p className="font-semibold text-md py-2 leading-8 text-zinc-800 opacity-80 dark:text-zinc-300">
-              I offer from a wide range of services, including brand design,
-              programming and teaching.
+              Abaixa estão algumas das pricipais tecnologias com que sei trabalhar
             </p>
           </div>
           <div className="lg:flex gap-10">
-            <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white  opacity-90 flex-1">
-              <Image src={design} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2  ">
-                Beautiful Designs
+            <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-zinc-100  opacity-90 flex-1">
+              <Image src={js} width={100} height={100} />
+              <h3 className="text-2xl font-bold pt-8 pb-2  ">
+                Front-End
               </h3>
-              <p className="py-2">
-                Creating elegant designs suited for your needs following core
-                design theory.
+              <p className="py-2 font-semibold opacity-80 text-lg">
+                Java scrip é um linguagem de programção amplemente usada para desenvolvimento de sistemas entre outras aplicações como:
               </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
+
+              <h4 className="py-4 text-teal-600 text-xl font-bold">Plataformas</h4>
+
+              <div className="flex justify-center items-center pb-3 gap-3">
+                <Image src={react} width={50} height={50} /><span className="text-gray-800 py-1 text-lg font-semibold">React-JS</span>
+              </div>
+
+              <div className="flex justify-center items-center pb-3 gap-3">
+                <Image src={react} width={50} height={50} /><span className="text-gray-800 py-1 text-lg font-semibold">React-JS</span>
+              </div>
+
+              <div className="flex justify-center items-center pb-3 gap-3">
+                <Image src={react} width={50} height={50} /><span className="text-gray-800 py-1 text-lg font-semibold">React-JS</span>
+              </div>
             </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <Image src={code} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2 ">
-                Code your dream project
+
+            <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-zinc-100  opacity-90 flex-1">
+              <Image src={js} width={100} height={100} />
+              <h3 className="text-2xl font-bold pt-8 pb-2  ">
+                Back-end
               </h3>
-              <p className="py-2">
-                Do you have an idea for your next great website? Let's make it a
-                reality.
+              <p className="py-2 font-semibold opacity-80 text-lg">
+                Java scrip é um linguagem de programção amplemente usada para desenvolvimento de sistemas entre outras aplicações como:
               </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
+
+              <h4 className="py-4 text-teal-600 text-xl font-bold">Plataformas</h4>
+
+              <div className="flex justify-center items-center pb-3 gap-3">
+                <Image src={react} width={50} height={50} /><span className="text-gray-800 py-1 text-lg font-semibold">React-JS</span>
+              </div>
+
+              <div className="flex justify-center items-center pb-3 gap-3">
+                <Image src={react} width={50} height={50} /><span className="text-gray-800 py-1 text-lg font-semibold">React-JS</span>
+              </div>
+
+              <div className="flex justify-center items-center pb-3 gap-3">
+                <Image src={react} width={50} height={50} /><span className="text-gray-800 py-1 text-lg font-semibold">React-JS</span>
+              </div>
             </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <Image src={consulting} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2 ">Consulting</h3>
-              <p className="py-2">
-                Are you interested in feedback for your current project? I can
-                give you tips and tricks to level it up.
+
+            <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-zinc-100  opacity-90 flex-1">
+              <Image src={js} width={100} height={100} />
+              <h3 className="text-2xl font-bold pt-8 pb-2  ">
+                DevOps
+              </h3>
+              <p className="py-2 font-semibold opacity-80 text-lg">
+                Java scrip é um linguagem de programção amplemente usada para desenvolvimento de sistemas entre outras aplicações como:
               </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
+
+              <h4 className="py-4 text-teal-600 text-xl font-bold">Plataformas</h4>
+
+              <div className="flex justify-center items-center pb-3 gap-3">
+                <Image src={react} width={50} height={50} /><span className="text-gray-800 py-1 text-lg font-semibold">React-JS</span>
+              </div>
+
+              <div className="flex justify-center items-center pb-3 gap-3">
+                <Image src={react} width={50} height={50} /><span className="text-gray-800 py-1 text-lg font-semibold">React-JS</span>
+              </div>
+
+              <div className="flex justify-center items-center pb-3 gap-3">
+                <Image src={react} width={50} height={50} /><span className="text-gray-800 py-1 text-lg font-semibold">React-JS</span>
+              </div>
             </div>
           </div>
         </section>
